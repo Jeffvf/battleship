5130b22f0185114615906d31443c9f9e25808cca
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-02 15:03:38 -0300

    feat: ship factory tests

---
+++ src/test/ship.test.js
@@ -0,0 +1,17 @@
+import Ship from '../ship'
+
+describe('Ship methods', () => {
+  let ship, destroyer;
+  beforeEach(() => {
+    ship = new Ship(1);
+    destroyer = new Ship(2);
+  });
+  test('Is sunk after n hits', () => {
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy;
+  });
+  test('Number of hits increases', () => {
+    destroyer.hit();
+    expect(destroyer.hits).toBe(1);
+  });
+});
