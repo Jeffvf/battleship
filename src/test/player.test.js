1e27e09ce6d14a50bed1b8548deba26b60503af3
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-04 20:15:29 -0300

    feat: player factory test

---
+++ src/test/player.test.js
@@ -0,0 +1,24 @@
+import Player from "../player";
+
+describe('Player methods', () => {
+  let player, computer;
+  beforeAll(() => {
+    player = new Player();
+    computer = new Player(true);
+  });
+  test('Player attacks Computer', () => {
+    player.attack(0, 0, computer.board);
+    expect(player.atkPositions.length).toBe(1);
+  });
+  test('Computer attacks player randomly', () => {
+    computer.autoAttack(player.board);
+    expect(computer.atkPositions.length).toBe(1);
+  });
+  test('Board is refreshed after attack', () => {
+    computer.board.placeShip([[0,2]]);
+    const newBoard = player.attack(0, 2, computer.board);
+    computer.setBoard = newBoard;
+
+    expect(computer.board.numberOfShips).toBe(0);
+  });
+})
