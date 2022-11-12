fa897abcdff52adc428a82debe542ed3dc995e33
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-12 15:53:27 -0300

    refactor: init game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './styles.css'
 import game from './game'
+import UI from './UI'
+import Player from './factory/player';
 
-game.start();
+const p1 = new Player();
+const p2 = game.initPlayer(true);
+UI.load(p1, p2);
